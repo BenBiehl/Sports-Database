c46d19811768e8afcf2bce1f3dd5ab1c6a9852e7
BenBiehl <benjaminpbiehl@gmail.com>
2024-10-24 15:44:56 -0500

    athlete class

--- main/models.py
+++ main/models.py
@@ -7,3 +7,17 @@ class User(models.Model):
     isAdmin = models.BooleanField(default=False)
     teamName = models.CharField(max_length=20)
     userView = models.IntegerField(default=1)
+
+class Athlete(models.Model):
+    id = models.IntegerField(primary_key=True)
+    height = models.CharField(max_length=5)
+    weight = models.CharField(max_length=10)
+    firstName = models.CharField(max_length=20)
+    mInit = models.CharField(max_length=1)
+    lastName = models.CharField(max_length=20)
+    birthDate = models.DateField()
+    age = models.IntegerField()
+    wins = models.IntegerField()
+    losses = models.IntegerField()
+    joinYear = models.IntegerField()
+    teamName = models.CharField(max_length=20)
