81971d11d9591ea3197629f7a8f6342a7d41a64c
BenBiehl <benjaminpbiehl@gmail.com>
2024-10-25 14:20:05 -0500

    different pages, not quite working yet

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.landing_page, name='landingpage')
+    path("", views.landing_page, name="landing_page"),
+    path("login_page", views.login_page, name="login_page"),
+    path("signup_page", views.signup_page, name="signup_page"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 
 from .models import User
 
+loggedIn = False
+
 def landing_page(request):
-    return render(request, "main/landingpage.html")
+    return render(request, "main/landing_page.html")
+
+def login_page(request):
+    return render(request, "main/login_page.html")
+
+def signup_page(request):
+    return render(request, "main/signup_page.html")
