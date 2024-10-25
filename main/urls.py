8f2d479acf5b99701d4d95d3459241434be7d678
BenBiehl <benjaminpbiehl@gmail.com>
2024-10-25 14:40:21 -0500

    ok navigation works now

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.landing_page, name="landing_page"),
-    path("login_page", views.login_page, name="login_page"),
-    path("signup_page", views.signup_page, name="signup_page"),
+    path("login_page/", views.login_page, name="login_page"),
+    path("signup_page/", views.signup_page, name="signup_page"),
 ]
