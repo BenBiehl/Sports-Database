cf4c6b9396b27d20f4f43737191e9bf8fce57b47
BenBiehl <benjaminpbiehl@gmail.com>
2024-10-31 16:18:45 -0500

    signup almost stylized

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,9 @@
 from django import forms
+from django.forms import TextInput
 
 class LogSignForm(forms.Form):
-    user_name = forms.CharField(label="User Name:", max_length=20)
-    password = forms.CharField(label="Password:", max_length=20)
+    user_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Name', 'style': 'width: 200px;'}))
+    password = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'placeholder': 'Password', 'style': 'width: 200px;'}))
 
 class ProfileForm(forms.Form):
     fav_team = forms.CharField(label="Add/Change Team", max_length=20)
